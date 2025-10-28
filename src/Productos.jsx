ad653cf0ac8409e72eb3ec5ac009ae7a0cd4fe48
Mateofelicia <d48011357@alumnos.itr.edu.ar>
2025-10-28 11:13:06 -0300

    Ejercicio 3 realizado

--- src/Productos.jsx
+++ src/Productos.jsx
@@ -1,24 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react"; //importaciones
 import "./Productos.css";
 
 export default function Productos() {
-  const [productos, setProductos] = useState([]);
+  const [productos, setProductos] = useState([]); //estado para guardar los productos
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products/") //nos enganchamos a la API
+      .then((res) => res.json())
+      .then((data) => setProductos(data))
+      .catch((error) => console.error("Error al obtener los productos:", error));
+
+  }, []);
 
 
   return (
-    <div className="productos-container">
+    <div className="productos-container"> 
       <h2 className="productos-title">Lista de productos</h2>
-      <div className="productos-grid">
-        {/* // Estructura sugerida para mostrar cada producto:
-        // <div className="producto">
-        //   <img src={producto.image} alt={producto.title} />
-        //   <h3>{producto.title}</h3>
-        //   <p className="categoria">Categoría: {producto.category}</p>
-        //   <p className="precio">${producto.price}</p>
-        //   <p className="rating">⭐ {producto.rating.rate} ({producto.rating.count} reseñas)</p>        
-        // </div> */}
+      {productos.length === 0 ? (
+        <p>Cargando productos...</p>
+      ) : (
+        <div className="productos-grid">
 
-      </div>
+          {productos.map((producto) => (
+            <div key={producto.id} className="producto-card">
+
+              <img className="producto-img" src={producto.image} alt={producto.title} />
+              <h3 className="producto-titulo">{producto.title}</h3>
+              <p className="producto-categoria">Categoría: {producto.category}</p>
+              <p className="producto-precio">${producto.price}</p>
+              <p className="producto-rating">⭐ {producto.rating.rate} ({producto.rating.count} reseñas)</p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
